350e6b172d47b514db8b6c7aaf980467eaa66f74
AsadtechWalk <asad.qayyum@techwalks.com>
2026-02-25 17:05:52 +0500

    added socket connectivity

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,16 @@
 import dotenv from "dotenv";
 dotenv.config();
-
+import { createServer } from "http";
 import app from "./app";
 import { connectDB } from "./config/db";
 const PORT = process.env.PORT ? Number(process.env.PORT) : 5000;
-
+import { initSocket } from "./socket";
 (async () => {
   try {
     await connectDB();
-    app.listen(PORT, () => {
+    const httpServer = createServer(app);
+    initSocket(httpServer);
+    httpServer.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
     });
   } catch (err) {
---
+++ src/socket.ts
@@ -0,0 +1,29 @@
+import { Socket } from "dgram";
+import { Server } from "socket.io";
+
+let io: Server;
+
+export const initSocket = (server: any) => {
+  io = new Server(server, {
+    cors: { origin: "*" },
+  });
+
+  io.on("connection", (socket) => {
+    console.log("User connected", socket.id);
+
+    socket.on("join", (userId) => {
+      socket.join(userId); // join user room
+    });
+
+    socket.on("disconnect", () => {
+      console.log("Disconnected");
+    });
+  });
+
+  return io;
+};
+
+export const getIO = () => {
+  if (!io) throw new Error("Socket not initialized");
+  return io;
+};
